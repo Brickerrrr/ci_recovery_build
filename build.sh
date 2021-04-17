4a6821b254f9176cae6733673e7c7e4a68cd4c66
Brock5555 <77067332+Brock5555@users.noreply.github.com>
2021-04-17 20:29:15 +0530

    Build for phoenix

--- build.sh
+++ build.sh
@@ -8,19 +8,19 @@ cd /tmp/recovery
 
 sudo apt install git -y
 
-repo init --depth=1 -u git://github.com/SHRP/platform_manifest_twrp_omni.git -b v3_10.0 -g default,-device,-mips,-darwin,-notdefault 
+repo init --depth=1 -u git://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-10.0 -g default,-device,-mips,-darwin,-notdefault 
 
 repo sync -j$(nproc --all)
 
-git clone https://github.com/Brock5555/device_realme_RMX2185 device/realme/RMX2185
+git clone https://github.com/hraj9258/twrp_phoenix.git -b test-oss device/xiaomi/phoenix
 
 rm -rf out
 
-. build/envsetup.sh && lunch omni_RMX2185-eng && export ALLOW_MISSING_DEPENDENCIES=true && export LC_ALL="C" && mka recoveryimage
+. build/envsetup.sh && lunch omni_phoenix-eng && export ALLOW_MISSING_DEPENDENCIES=true && export LC_ALL="C" && mka recoveryimage
 
 # Upload zips & recovery.img (U can improvise lateron adding telegram support etc etc)
 
-cd out/target/product/RMX2185
+cd out/target/product/phoenix
 
 curl -sL https://git.io/file-transfer | sh 
 
